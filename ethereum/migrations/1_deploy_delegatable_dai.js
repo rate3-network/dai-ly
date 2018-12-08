90bb616ce0bfb650e50d3d7beb0e27086de5bbb5
Waihon Chee <imwaihon@gmail.com>
2018-12-08 20:37:38 +0800

    Remove helpers

--- ethereum/migrations/1_deploy_delegatable_dai.js
+++
@@ -1,9 +0,0 @@
-const DelegatableDai = artifacts.require('./DelegatableDai.sol');
-
-const deployContracts = async (deployer, network, accounts) => {
-  await deployer.deploy(DelegatableDai);
-};
-
-module.exports = function deploy(deployer, network, accounts) {
-    return deployer.then(() => deployContracts(deployer, network, accounts));
-};
