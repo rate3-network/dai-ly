5125e7aa36c36ee0cd431d9e44dcefe1c3ad7829
Waihon Chee <imwaihon@gmail.com>
2018-12-08 18:11:51 +0800

    Added ERC865

---
+++ ethereum/contracts/ERC865.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @title ERC865 Token
+ *
+ * ERC865 Token allows users paying transfers in tokens instead of gas
+ * https://github.com/ethereum/EIPs/issues/865
+ *
+ */
+
+contract ERC865 {
+
+    function transferPreSigned(
+        bytes _signature,
+        address _to,
+        uint256 _value,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function approvePreSigned(
+        bytes _signature,
+        address _spender,
+        uint256 _value,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function increaseApprovalPreSigned(
+        bytes _signature,
+        address _spender,
+        uint256 _addedValue,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function decreaseApprovalPreSigned(
+        bytes _signature,
+        address _spender,
+        uint256 _subtractedValue,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function transferFromPreSigned(
+        bytes _signature,
+        address _from,
+        address _to,
+        uint256 _value,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+}
